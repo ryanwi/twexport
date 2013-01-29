0c42018e468188e870e9ea2dd1f3ec2de556d57a
Ryan Williams <rwilliamz@gmail.com>
2013-01-28 22:32:43 -0800

    fix spacing

--- spec/twexport_spec.rb
+++ spec/twexport_spec.rb
@@ -2,8 +2,8 @@ require 'rspec'
 require_relative '../twexport.rb'
 
 describe Twexport do 
-	it "exports profile information from a twitter list to a file" do
-	  twexport = Twexport.new
-	  twexport.save("list.csv")
-	end
+  it "exports profile information from a twitter list to a file" do
+    twexport = Twexport.new
+    twexport.save("list.csv")
+  end
 end
