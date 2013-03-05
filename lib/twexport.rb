400d1fb6c4e40ca95290d8676c74b378975d0dd6
Ryan Williams <rwilliamz@gmail.com>
2013-03-04 23:04:54 -0800

    clean up tabs

--- lib/twexport.rb
+++ lib/twexport.rb
@@ -3,18 +3,18 @@ require 'twexport/webpage'
 
 module Twexport
 
-	class Exporter
+  class Exporter
 
-		def save
+    def save
       CSV.open(path, "wb", {:force_quotes=>true}) do |csv|
         csv << ["name", "screen_name", "location", "url", "description", "followers", "following", "listed"]
         @users.each do |user|
           csv << [user[:name], user[:screen_name], user[:location], user[:url], user[:description], 
-            user[:followers_count], user[:friends_count], user[:listed_count]]
+          user[:followers_count], user[:friends_count], user[:listed_count]]
         end
       end
-		end
+    end
 
-	end
+  end
 
 end
